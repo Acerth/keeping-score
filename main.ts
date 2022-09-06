670e9f28ee5c50436274a029b3b65deb09881a68
Acerth <112982042+Acerth@users.noreply.github.com>
2022-09-06 14:27:18 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,7 +17,15 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    basic.showString("A -")
+    basic.showString("" + (A_Score))
+    basic.clearScreen()
+    basic.showString("B -")
+    basic.showString("" + (B_Score))
+    basic.clearScreen()
+    basic.showString("Games Tied -")
+    basic.showString("" + (Point_Tied))
+    basic.clearScreen()
 })
 let Point_Tied = 0
 let B_Score = 0
@@ -25,6 +33,3 @@ let A_Score = 0
 A_Score = 0
 B_Score = 0
 Point_Tied = 0
-basic.forever(function () {
-	
-})
