8522c520aa0e220a332d091a721c0ba63fda8956
Acerth <112982042+Acerth@users.noreply.github.com>
2022-09-06 14:13:16 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    A_Score += 1
+    basic.showString("A -")
+    basic.showString("" + (A_Score))
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    Point_Tied += 1
+    basic.showString("Games Tied -")
+    basic.showString("" + (Point_Tied))
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    B_Score += 1
+    basic.showString("B -")
+    basic.showString("" + (B_Score))
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+let Point_Tied = 0
+let B_Score = 0
+let A_Score = 0
+A_Score = 0
+B_Score = 0
+Point_Tied = 0
+basic.forever(function () {
+	
+})
